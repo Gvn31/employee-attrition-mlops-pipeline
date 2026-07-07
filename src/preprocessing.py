3a358410e8a263a7905b08355722c9f22f2420d2
Gvn31 <jivin.2201@gmail.com>
2026-07-07 11:38:51 +0530

    Add data preprocessing module

---
+++ src/preprocessing.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+
+#Load Dataset
+df=pd.read_csv(r"../data/raw/emp_attrition_csv.csv")
+
+#Drop Duplicates
+df.drop_duplicates(inplace=True)
+
+#Handle Missing Values
+
+df['Distance from Home']=df['Distance from Home'].fillna(df['Distance from Home'].median())
+df['Company Tenure (In Months)']=df['Company Tenure (In Months)'].fillna(df['Company Tenure (In Months)'].median())
+
+# #Spelling Correction
+df['Education Level']=df['Education Level'].str.strip()
+df['Education Level']=df['Education Level'].str.replace("Masterâ€™s Degree","Master's Degree")
+df['Education Level']=df['Education Level'].str.replace("Bachelorâ€™s Degree","Bachelor's Degree")
+
+
+#Save Cleaned Dataset
+df.to_csv(r"../data/processed/emp_attrition_cleaned.csv",index=False)
+
