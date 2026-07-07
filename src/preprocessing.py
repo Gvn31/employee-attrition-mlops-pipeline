c88f1e6a957dcde082dbb2f2e8930a8243e868d9
Gvn31 <jivin.2201@gmail.com>
2026-07-07 20:01:33 +0530

    Save cleaned Data

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,26 +1,29 @@
 import numpy as np
 import pandas as pd
+try:
+    
+     #Load Dataset
+    df=pd.read_csv(r"../data/raw/emp_attrition_csv.csv")
 
-#Load Dataset
-df=pd.read_csv(r"../data/raw/emp_attrition_csv.csv")
+    #Drop Duplicates
+    df.drop_duplicates(inplace=True)
 
-#Drop Duplicates
-df.drop_duplicates(inplace=True)
+    #Handle Missing Values
+    df['Distance from Home']=df['Distance from Home'].fillna(df['Distance from Home'].median())
+    df['Company Tenure (In Months)']=df['Company Tenure (In Months)'].fillna(df['Company Tenure (In Months)'].median())
 
-#Handle Missing Values
+     #Spelling Correction
+    df['Education Level']=df['Education Level'].str.strip()
+    df['Education Level']=df['Education Level'].str.replace("Masterâ€™s Degree","Master's Degree")
+    df['Education Level']=df['Education Level'].str.replace("Bachelorâ€™s Degree","Bachelor's Degree")
 
-df['Distance from Home']=df['Distance from Home'].fillna(df['Distance from Home'].median())
-df['Company Tenure (In Months)']=df['Company Tenure (In Months)'].fillna(df['Company Tenure (In Months)'].median())
-
-#Spelling Correction
-df['Education Level']=df['Education Level'].str.strip()
-df['Education Level']=df['Education Level'].str.replace("Masterâ€™s Degree","Master's Degree")
-df['Education Level']=df['Education Level'].str.replace("Bachelorâ€™s Degree","Bachelor's Degree")
-
-#Drop Irrelevant Features
-df.drop(['Employee ID'],axis=1,inplace=True)
+    #Drop Irrelevant Features
+    df.drop(['Employee ID'],axis=1,inplace=True)
 
 
-#Save Cleaned Dataset
-df.to_csv(r"../data/processed/emp_attrition_cleaned.csv",index=False)
+    #Save Cleaned Dataset
+    df.to_csv(r"../data/processed/emp_attrition_cleaned.csv",index=False)
+    
+except Exception as e:
+    print(f"Error: {e}")
 
